2ae66fc2cc7a16b280f9a31ef70e1b2bee899789
Jordan Epps <jordanepps@gmail.com>
2020-02-04 10:06:53 -0500

    Rework front end with dropzone and remove all server methods

--- server/src/app.js
+++ server/src/app.js
@@ -5,64 +5,53 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 // const multer = require('multer');
-// const upload = multer({ dest: './uploads/' });
-const formidable = require('formidable');
-const fs = require('fs');
+
+// const formidable = require('formidable');
+// const fs = require('fs');
 
 const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+// const upload = multer({
+//   dest: 'uploads/',
+//   filename: (req, file, next) => console.log(file)
+// });
 
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
+  res.send('Image Recipe Test Server');
 });
 
-app.post('/upload', (req, res, next) => {
-  //   upload(req, res, function(err) {
-  //     if (err) {
-  //       return res.end('Error uploading file.');
-  //     }
-  //     res.end('File is uploaded');
-  //   });
-  //   console.log(req.files);
-  //   console.log(req);
-  //   res.send('Good');
+// app.post('/upload', (req, res, next) => {
+//   let form = new formidable.IncomingForm().parse(req);
+//   form.on('fileBegin', function(name, file) {
+//     file.path = __dirname + '/uploads/' + file.name;
+//   });
+//   form.on('file', function(name, file) {
+//     console.log('Uploaded ' + file.name);
+//   });
 
-  // new formidable.IncomingForm().parse(req, (err, fields, files) => {
-  //   if (err) {
-  //     console.error('Error', err);
-  //     throw err;
-  //   }
-  //   console.log('Fields', fields);
-  //   console.log('Files', files);
-  //   for (const file of Object.entries(files)) {
-  //     console.log(file);
-  //   }
-  // });
+// });
 
-  let form = new formidable.IncomingForm().parse(req);
+/**********
+   File Upload With Multer 
+   **********/
 
-  // form.parse(req);
+// app.post('/upload', upload.single('foodImage'), (req, res, next) => {
+//   res.send('ok');
+// });
 
-  form.on('fileBegin', function(name, file) {
-    file.path = __dirname + '/uploads/' + file.name;
-  });
-
-  form.on('file', function(name, file) {
-    console.log('Uploaded ' + file.name);
-  });
-
-  res.sendFile(__dirname + '/index.html');
-});
+/* GOOGLE CLOUD UPLOAD */
+app.post('/upload', (req, res, next) => {});
 
 app.use(function errorHandler(error, req, res, next) {
   let response =
     NODE_ENV === 'production'
-      ? { error: { message: 'server error' } }
+      ? { error: 'server error' }
       : { message: error.message, error };
   res.status(500).json(response);
 });
